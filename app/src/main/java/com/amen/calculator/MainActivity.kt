6d04495c4d74b956faebdf49060e49c68c1f4b17
Amen <aymantauhid2@gmail.com>
2022-03-09 02:14:01 +0530

    UI part has been developed, certain changes will be brought later to the UI such as DarkMode/LightMode switching functionality, also better animations. All of this after the back-end part is complete.

--- app/src/main/java/com/amen/calculator/MainActivity.kt
+++ app/src/main/java/com/amen/calculator/MainActivity.kt
@@ -2,10 +2,20 @@ package com.amen.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private var outputView: TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        outputView = findViewById(R.id.Output_View)
     }
+
 }
